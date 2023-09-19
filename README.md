cc1a8b16ee7c61a3ca9c2a80fc661a983db766c4
lake <102535481@qq.com>
2023-09-19 16:30:38 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # go-hbase
 go 读取hbase数据
-
+# hbase thrift2运行
+```shell
+bin/hbase-daemon.sh start thrift2
+```
 # 打包
 ```shell
 CGO_ENABLED=1 go build -ldflags '-linkmode "external" -extldflags "-static"' main.go
--- main.go
+++ main.go
@@ -1,31 +1,110 @@
 package main
 
 import (
-	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"github.com/apache/thrift/lib/go/thrift"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"go-hbase/hbase"
+	"os"
 	"time"
 )
 
+func MD5(str string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(str))
+	hashBytes := hasher.Sum(nil)
+	return hex.EncodeToString(hashBytes)
+}
+
+type UserQuery struct {
+	UID  *string  `json:"uid"`
+	UIDS []string `json:"uids" binding:"lte=100"`
+}
+
 func main() {
-	ctx := context.Background()
+	domain := os.Getenv("domain")
+	if domain == "" {
+		domain = "127.0.0.1:9090"
+	}
+	fmt.Println("hbase domain:", domain)
 	protocolFactory := thrift.NewTBinaryProtocolFactoryConf(&thrift.TConfiguration{
 		ConnectTimeout: 3 * time.Second,
 		SocketTimeout:  3 * time.Second,
 	})
-	transport := thrift.NewTSocketConf("127.0.0.1:9090", &thrift.TConfiguration{})
+	transport := thrift.NewTSocketConf(domain, &thrift.TConfiguration{})
 	client := hbase.NewTHBaseServiceClientFactory(transport, protocolFactory)
 	if err := transport.Open(); err != nil {
 		panic(err)
 	}
 	userTable := []byte("USER_TABLE_V3")
 
-	result, err := client.Get(ctx, userTable, &hbase.TGet{Row: []byte("85eebccd895c279a1d9f5853c20fe874")})
-	if err != nil {
-		panic(err)
-	}
-	for _, column := range result.ColumnValues {
-		fmt.Printf("%s:%s\n", column.Qualifier, column.Value)
-	}
+	r := gin.New()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Token"},
+		AllowCredentials: true,
+	}))
+
+	r.GET("/user", func(c *gin.Context) {
+		ctx := c.Request.Context()
+		var userQuery UserQuery
+		err := c.BindJSON(&userQuery)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"msg": err.Error(),
+			})
+			return
+		}
+		if userQuery.UID != nil {
+			result, err := client.Get(ctx, userTable, &hbase.TGet{Row: []byte(MD5(*userQuery.UID))})
+			if err != nil {
+				c.JSON(200, gin.H{
+					"msg": err.Error(),
+				})
+				return
+			}
+			data := make(map[string]string)
+			for _, column := range result.ColumnValues {
+				data[string(column.Qualifier)] = string(column.Value)
+			}
+			c.JSON(200, gin.H{
+				"code": 1,
+				"data": data,
+			})
+		} else if userQuery.UIDS != nil {
+			var gets []*hbase.TGet
+			for _, uid := range userQuery.UIDS {
+				gets = append(gets, &hbase.TGet{Row: []byte(MD5(uid))})
+			}
+			results, err := client.GetMultiple(ctx, userTable, gets)
+			if err != nil {
+				c.JSON(200, gin.H{
+					"msg": err.Error(),
+				})
+				return
+			}
+			var datas []map[string]string
+			for _, result := range results {
+				maps := make(map[string]string)
+				for _, column := range result.ColumnValues {
+					maps[string(column.Qualifier)] = string(column.Value)
+				}
+				datas = append(datas, maps)
+			}
+			c.JSON(200, gin.H{
+				"code": 1,
+				"data": datas,
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"msg": "uid或者uids必填其中一个参数",
+			})
+		}
+	})
+
+	r.Run(":8000")
 }
